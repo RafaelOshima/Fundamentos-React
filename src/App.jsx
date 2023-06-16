33f3ff43300cd786b675188e6f355a9078e13190
Rafael <yoshiaki_oshima@outlook.com>
2023-06-16 20:03:50 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Header } from './components/Header';
 import {Post} from './post';
+
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
       
-      <Post 
-        author="Rafael Oshima"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, dolorem odio? Ipsum fuga voluptatum asperiores dolore? Sunt sapiente voluptate beatae maxime repellat, excepturi obcaecati eaque optio architecto voluptates sint tempora." 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Rafael Oshima"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, dolorem odio? Ipsum fuga voluptatum asperiores dolore? Sunt sapiente voluptate beatae maxime repellat, excepturi obcaecati eaque optio architecto voluptates sint tempora." 
+          />
 
-      <Post
-        author="Vinícius Oshima"
-        content="Um novo post muito legal"
-      />
+          <Post
+            author="Vinícius Oshima"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
     </div>
   )
 }
