c2e16b3277e29e499c6d4a77d6517d0f9fc21eec
Rafael <yoshiaki_oshima@outlook.com>
2023-06-17 12:27:36 -0300

    Estrutura da aplicação - Finalizando Sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from '@phosphor-icons/react'
+
 import styles from './sidebar.module.css'
 
 export function Sidebar() {
@@ -6,12 +8,15 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/RafaelOshima.png" />
+
         <strong>Rafael Oshima</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
