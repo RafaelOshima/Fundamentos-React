6e5c604e2fc6ddc9695ea959285fdcc331b5412f
Rafael <yoshiaki_oshima@outlook.com>
2023-06-20 19:28:54 -0300

    Estrutura da aplicação - Componente: Avatar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from '@phosphor-icons/react'
 
 import styles from './sidebar.module.css'
+import { Avatar } from './Avatar'
 
 export function Sidebar() {
   return (
@@ -8,7 +9,7 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/RafaelOshima.png" />
+        <Avatar src="https://github.com/RafaelOshima.png" />
 
         <strong>Rafael Oshima</strong>
         <span>Web Developer</span>
