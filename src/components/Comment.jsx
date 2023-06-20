c0f1aa2842b00f54004d66746131f0e73a8b0e0b
Rafael <yoshiaki_oshima@outlook.com>
2023-06-20 11:16:52 -0300

    Estrutura da aplicação - Componente: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/RafaelOshima.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Rafael Oshima</strong>
+              <time title='20 de junho às 10:56' dateTime='2023-06-20 10:56:30'>Cerca de 1h atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <ThumbsUp />
+          Aplaudir <span>20</span>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+
